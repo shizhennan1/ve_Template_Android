d996ae5fd521d7514a3f6b70f67bf4cb6dc59886
imurluck <1289042324@qq.com>
2021-11-09 21:06:18 +0800

    [feature]: h5 demo of version 1.1.0

--- app/src/main/java/com/volcengine/mars/app/LaunchApplication.java
+++ app/src/main/java/com/volcengine/mars/app/LaunchApplication.java
@@ -3,13 +3,23 @@ package com.volcengine.mars.app;
 import android.app.ActivityManager;
 import android.app.Application;
 import android.content.Context;
+import android.widget.Toast;
+
 import com.bytedance.lego.init.InitScheduler;
 import com.bytedance.lego.init.config.TaskConfig;
 import com.bytedance.lego.init.model.InitPeriod;
 import com.bytedance.lego.init.monitor.InitMonitor;
+import com.bytedance.lynx.webview.TTWebSdk;
+import com.bytedance.veh5.VEH5;
+import com.bytedance.veh5.config.VEH5Config;
 import com.volcengine.mars.activity.ActivityStack;
 import com.volcengine.mars.utils.AppUtils;
+import com.volcengine.onekit.OneKitApp;
+
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.regex.Pattern;
 
 public class LaunchApplication extends Application {
 
@@ -45,6 +55,7 @@ public class LaunchApplication extends Application {
         InitScheduler.config(config);
 
         super.attachBaseContext(base);
+        OneKitApp.initialize(this);
         MPLaunch.INSTANCE.onPeriod(sApplication, InitPeriod.APP_ATTACHBASE2SUPER);
         MPLaunch.INSTANCE.onPeriod(sApplication, InitPeriod.APP_SUPER2ATTACHBASEEND);
     }
@@ -66,10 +77,44 @@ public class LaunchApplication extends Application {
 
     @Override
     public void onCreate() {
+        initializeH5Service();
         ActivityStack.init(this);
         MPLaunch.INSTANCE.onPeriod(sApplication, InitPeriod.APP_ONCREATE2SUPER);
         super.onCreate();
         MPLaunch.INSTANCE.onPeriod(sApplication, InitPeriod.APP_SUPER2ONCREATEEND);
+    }
+
+    private void initializeH5Service() {
+        List<Pattern> cachePrefixes = new ArrayList<>();
+        cachePrefixes.add(Pattern.compile("https://mars-jsbridge.goofy-web.bytedance.com/"));
+        cachePrefixes.add(Pattern.compile("https://sf6-scmcdn-tos.pstatp.com/goofy/"));
+
+        List<String> internalCacheDirs = new ArrayList<>();
+        internalCacheDirs.add("offline-resources");
+
+        VEH5.INSTANCE.initialize(
+                this,
+                new VEH5Config.Builder()
+                        .enableDebug(true)
+                        .setDeviceId("7135843788734")
+                        .setOfflineResourceAccessKey("d3d71ae74f5383ec33b4fd85b298f50f")
+                        .setOfflineResourceUrlPrefixes(cachePrefixes)
+                        .setInternalCacheDirs(internalCacheDirs)
+                        .enableKernel(true)
+                        .setKernelInitListener(new TTWebSdk.InitListener() {
+                            @Override
+                            public void onDownloadFinished() {
+                                super.onDownloadFinished();
+                                Toast.makeText(LaunchApplication.this, "内核下载完成", Toast.LENGTH_SHORT).show();
+                            }
+
+                            @Override
+                            public void onSoFileUpdateFinished() {
+                                super.onSoFileUpdateFinished();
+                                Toast.makeText(LaunchApplication.this, "内核下载完成", Toast.LENGTH_SHORT).show();
+                            }
+                        })
+        );
     }
 
     /**
--- demos/h5_service/src/main/java/com/volcengine/mars/demo/h5/H5MainActivity.kt
+++ demos/h5_service/src/main/java/com/volcengine/mars/demo/h5/H5MainActivity.kt
@@ -4,14 +4,15 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
+import com.bytedance.lynx.reader.TTReader
+import com.bytedance.lynx.webview.TTWebSdk
 import com.bytedance.veh5.VEH5
 import com.bytedance.veh5.config.VEH5Config
 import com.bytedance.veh5.debug.DebugTools
-import kotlinx.android.synthetic.main.activity_h5_main.openDevelopActivityBtn
-import kotlinx.android.synthetic.main.activity_h5_main.openExampleOfflineActivityBtn
-import kotlinx.android.synthetic.main.activity_h5_main.openExampleOnlineActivityBtn
+import kotlinx.android.synthetic.main.activity_h5_main.*
 import java.util.regex.Pattern
 
 class H5MainActivity : AppCompatActivity() {
@@ -19,11 +20,24 @@ class H5MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         VEH5.initialize(
+            application,
             VEH5Config.Builder()
                 .enableDebug(true)
                 .setDeviceId("7135843788734")
                 .setOfflineResourceAccessKey("d3d71ae74f5383ec33b4fd85b298f50f")
                 .setOfflineResourceUrlPrefixes(getCachePrefixes())
+                .setInternalCacheDirs(listOf("offline-resources"))
+                .enableKernel(true)
+                .setKernelInitListener(object : TTWebSdk.InitListener() {
+                    override fun onDownloadFinished() {
+                        super.onDownloadFinished()
+                        Toast.makeText(this@H5MainActivity, "内核下载完成", Toast.LENGTH_SHORT).show()
+                    }
+
+                    override fun onSoFileUpdateFinished() {
+                        Toast.makeText(this@H5MainActivity, "内核更新完成", Toast.LENGTH_SHORT).show()
+                    }
+                })
         )
         setContentView(R.layout.activity_h5_main)
 
@@ -41,6 +55,10 @@ class H5MainActivity : AppCompatActivity() {
             VEH5.getGlobalController()?.setOfflineResourcesEnable(true)
             DebugTools.clearOfflineResourcesLoadRecord()
             startActivity(Intent(this, H5ExampleActivity::class.java))
+        }
+
+        exampleTTWebViewBtn.setOnClickListener {
+            startActivity(Intent(this, TTWebViewActivity::class.java))
         }
 
         requestPermissions()
